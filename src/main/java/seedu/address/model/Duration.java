9b3d056fda4340545fb441f1d32fc9ebaf334d8c
Tianyi <42670338+tysng@users.noreply.github.com>
2019-10-09 16:07:28 +0800

    Complete function and test

--- src/main/java/seedu/address/model/Duration.java
+++ src/main/java/seedu/address/model/Duration.java
@@ -4,23 +4,50 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-public class Duration {
+/**
+ * A Duration contains a start time and an end time.
+ */
+public class Duration implements Comparable<Duration> {
 
     private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HHmm");
     private LocalTime start;
     private LocalTime end;
 
-    public Duration(LocalTime start, LocalTime end) {
-        this.start = start;
-        this.end = end;
+    private Duration(LocalTime firstTime, LocalTime secondTime) {
+        boolean isFirstTimeEarly = firstTime.compareTo(secondTime) < 0;
+        this.start = isFirstTimeEarly ? firstTime : secondTime;
+        this.end = isFirstTimeEarly ? secondTime : firstTime;
     }
 
-    public static Duration parse(String start, String end) {
+    /**
+     * Builds duration from two text representations of time. The method decides the start and end time of the duration
+     * based on the input.
+     * <p>
+     * This methods appends zeros to the front of the input, if
+     * the input is less than 4 digits. For example, "900" will be changed into "0900".
+     *
+     * @param firstTime one of the start or end time
+     * @param secondTime the other start or end time
+     * @return the duration with the specified start and end time
+     */
+    public static Duration parse(String firstTime, String secondTime) {
         // append front with zero
-        String startTime = String.format("%04d", Integer.parseInt(start));
-        String endTime = String.format("%04d", Integer.parseInt(end));
+        String first = String.format("%04d", Integer.parseInt(firstTime));
+        String second = String.format("%04d", Integer.parseInt(secondTime));
 
-        return new Duration (LocalTime.parse(startTime, TIME_FORMAT), LocalTime.parse(endTime, TIME_FORMAT));
+        return new Duration(LocalTime.parse(first, TIME_FORMAT), LocalTime.parse(second, TIME_FORMAT));
+    }
+
+    /**
+     * Checks whether the two durations overlap.
+     * @param other the other duration
+     * @return whether they overlap
+     */
+    public boolean overlaps(Duration other) {
+        Duration early = this.compareTo(other) > 0 ? other : this;
+        Duration late = this.compareTo(other) > 0 ? this : other;
+
+        return early.getEnd().compareTo(late.getStart()) > 0;
     }
 
     public LocalTime getEnd() {
@@ -47,5 +74,12 @@ public class Duration {
     @Override
     public int hashCode() {
         return Objects.hash(this.start, this.end);
+    }
+
+    @Override
+    public int compareTo(Duration o) {
+        return !this.getStart().equals(o.getStart())
+                ? this.getStart().compareTo(o.getStart())
+                : this.getEnd().compareTo(o.getEnd());
     }
 }
--- src/test/java/seedu/address/model/DurationTest.java
+++ src/test/java/seedu/address/model/DurationTest.java
@@ -1,22 +1,47 @@
 package seedu.address.model;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.model.Duration;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class DurationTest {
 
     @Test
     void parse_nonZeroPaddedInput_returnsCorrectTime() {
-        String start = "920";
-        String end =  "1600";
-
-        Duration duration = Duration.parse(start, end);
+        Duration duration = Duration.parse("920", "1600");
 
         assertEquals(9, duration.getStart().getHour());
         assertEquals(20, duration.getStart().getMinute());
         assertEquals(16, duration.getEnd().getHour());
         assertEquals(0, duration.getEnd().getMinute());
+    }
+
+    @Test
+    void overlaps_partialOverlap_returnsTrue() {
+        Duration duration1 = Duration.parse("0010", "0101");
+        Duration duration2 = Duration.parse("0100", "0200");
+
+        assertTrue(duration1.overlaps(duration2));
+        assertTrue(duration2.overlaps(duration1));
+    }
+
+    @Test
+    void overlaps_fullOverlap_returnsTrue() {
+        Duration duration1 = Duration.parse("0001", "1900");
+        Duration duration2 = Duration.parse("0030", "0200");
+
+        assertTrue(duration1.overlaps(duration2));
+        assertTrue(duration2.overlaps(duration1));
+    }
+
+    @Test
+    void overlaps_discreteTime_returnsFalse() {
+        Duration duration1 = Duration.parse("0010", "0100");
+        Duration duration2 = Duration.parse("0100", "0200");
+
+        assertFalse(duration1.overlaps(duration2));
+        assertFalse(duration2.overlaps(duration1));
     }
 }
