80e87a259cbe6bd7eb76016767d46fe7f4b99cc5
ArgVampir <kris_.k@hotmail.com>
2019-10-09 23:50:06 +0800

    Added java docs

--- src/main/java/seedu/address/model/Goods.java
+++ src/main/java/seedu/address/model/Goods.java
@@ -1,5 +1,9 @@
 package seedu.address.model;
 
+/**
+ * Deals with the name of the product to be delivered.
+ */
+
 public class Goods {
 
     private String name;
@@ -8,10 +12,17 @@ public class Goods {
         this.name = name;
     }
 
+    /**
+     * Edits the Name of the goods.
+     *
+     * @param newName The new name to be changed into
+     */
+
     public void setName(String newName) {
         this.name = newName;
     }
 
+    @Override
     public String toString() {
         return name;
     }
