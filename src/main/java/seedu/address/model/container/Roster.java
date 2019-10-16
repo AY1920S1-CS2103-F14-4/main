8029843c5a85afe82f27d1656699e52f94c46a0e
Tianyi <42670338+tysng@users.noreply.github.com>
2019-10-16 15:31:00 +0800

    Initial attempt to change to generics

---
+++ src/main/java/seedu/address/model/container/Roster.java
@@ -0,0 +1,120 @@
+package seedu.address.model.container;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import javafx.collections.ObservableList;
+
+import seedu.address.model.person.Person;
+
+/**
+ * Wraps all data at the address-book level
+ * Duplicates are not allowed (by .isSamePerson comparison)
+ */
+public class Roster<T extends Person> implements ReadOnlyRoster<T> {
+
+    private final UniqueRoster<T> persons;
+
+    /*
+     * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
+     * between constructors. See https://docs.oracle.com/javase/tutorial/java/javaOO/initial.html
+     *
+     * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
+     *   among constructors.
+     */ {
+        persons = new UniqueRoster<>();
+    }
+
+    public Roster() {
+    }
+
+    /**
+     * Creates an AddressBook using the Persons in the {@code toBeCopied}
+     */
+    public Roster(ReadOnlyRoster<T> toBeCopied) {
+        this();
+        resetData(toBeCopied);
+    }
+
+    //// list overwrite operations
+
+    /**
+     * Replaces the contents of the person list with {@code persons}.
+     * {@code persons} must not contain duplicate persons.
+     */
+    public void setPersons(List<T> persons) {
+        this.persons.setPersons(persons);
+    }
+
+    /**
+     * Resets the existing data of this {@code AddressBook} with {@code newData}.
+     */
+    public void resetData(ReadOnlyRoster<T> newData) {
+        requireNonNull(newData);
+
+        setPersons(newData.getPersonList());
+    }
+
+    //// person-level operations
+
+    /**
+     * Returns true if a person with the same identity as {@code person} exists in the address book.
+     */
+    public boolean hasPerson(T person) {
+        requireNonNull(person);
+        return persons.contains(person);
+    }
+
+    /**
+     * Adds a person to the address book.
+     * The person must not already exist in the address book.
+     */
+    public void addPerson(T p) {
+        persons.add(p);
+    }
+
+    /**
+     * Replaces the given person {@code target} in the list with {@code editedPerson}.
+     * {@code target} must exist in the address book.
+     * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
+     */
+    public void setPerson(T target, T editedPerson) {
+        requireNonNull(editedPerson);
+
+        persons.setPerson(target, editedPerson);
+    }
+
+    /**
+     * Removes {@code key} from this {@code AddressBook}.
+     * {@code key} must exist in the address book.
+     */
+    public void removePerson(T key) {
+        persons.remove(key);
+    }
+
+    //// util methods
+
+    @Override
+    public String toString() {
+        return persons.asUnmodifiableObservableList().size() + " persons";
+        // TODO: refine later
+    }
+
+    @Override
+    public ObservableList<T> getPersonList() {
+        return persons.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Roster // instanceof handles nulls
+                && persons.equals(((Roster) other).persons));
+    }
+
+    @Override
+    public int hashCode() {
+        return persons.hashCode();
+    }
+}
