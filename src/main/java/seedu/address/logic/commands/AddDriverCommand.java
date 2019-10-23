32ee31888b2575a68562f31d346449321f2b77e2
SamYJ2606 <e0002804@u.nus.edu>
2019-10-23 23:55:11 +0800

    Changes made

--- src/main/java/seedu/address/logic/commands/AddDriverCommand.java
+++ src/main/java/seedu/address/logic/commands/AddDriverCommand.java
@@ -2,15 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_CUSTOMER;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DRIVER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Customer;
 import seedu.address.model.person.Driver;
 import seedu.address.model.person.Person;
 
@@ -19,40 +17,37 @@ import seedu.address.model.person.Person;
 /**
  * Adds a person to the address book.
  */
-public class AddCustomerDriverCommand extends Command {
+public class AddDriverCommand extends Command {
 
-    public static final String COMMAND_WORD = "add";
+    public static final String COMMAND_WORD = "addD";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a customer to the address book. "
             + "Parameters: "
-            + PREFIX_CUSTOMER + "CUSTOMER NAME | "
-            + PREFIX_DRIVER + " DRIVER NAME | "
+            + PREFIX_NAME + " DRIVER NAME "
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_CUSTOMER + "John Doe "
+            + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "owesMoney";
 
-    public static final String MESSAGE_SUCCESS = "New %1$s added: %2$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This %1$s already exists.";
+    public static final String MESSAGE_SUCCESS = "New Driver added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This Driver already exists.";
 
     private final Person toAdd;
-    private final String className;
     private final int toAddId;
 
     /**
      * Creates an AddCommand to add the specified {@code Person}
      */
-    public AddCustomerDriverCommand(Person person) {
+    public AddDriverCommand(Person person) {
         requireNonNull(person);
         toAdd = person;
-        this.className = person.getClass().getSimpleName();
         this.toAddId = person.getId();
     }
 
@@ -60,15 +55,11 @@ public class AddCustomerDriverCommand extends Command {
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
 
-        if (model.hasCustomer(toAddId) || model.hasDriver(toAddId)) {
-            throw new CommandException(String.format(MESSAGE_DUPLICATE_PERSON, className));
+        if (model.hasDriver(toAddId)) {
+            throw new CommandException(String.format(MESSAGE_DUPLICATE_PERSON));
         }
 
-        if (className.equals(Customer.class.getSimpleName())) {
-            model.addCustomer((Customer) toAdd);
-        } else if (className.equals(Driver.class.getSimpleName())) {
-            model.addDriver((Driver) toAdd);
-        }
-        return new CommandResult(String.format(MESSAGE_SUCCESS, className, toAdd));
+        model.addDriver((Driver) toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
     }
 }
--- src/main/java/seedu/address/logic/parser/AddDriverCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddDriverCommandParser.java
@@ -2,8 +2,6 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_CUSTOMER;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DRIVER;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -13,14 +11,12 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import seedu.address.logic.commands.AddCustomerDriverCommand;
+import seedu.address.logic.commands.AddDriverCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Address;
-import seedu.address.model.person.Customer;
 import seedu.address.model.person.Driver;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
@@ -28,7 +24,7 @@ import seedu.address.model.tag.Tag;
 /**
  * Parses input arguments and creates a new AddCustomerDriverCommand object
  */
-public class AddCustomerDriverParser implements Parser<AddCustomerDriverCommand> {
+public class AddDriverCommandParser implements Parser<AddDriverCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the AddCustomerDriverCommand
@@ -36,17 +32,14 @@ public class AddCustomerDriverParser implements Parser<AddCustomerDriverCommand>
      *
      * @throws ParseException if the user input does not conform the expected format
      */
-    public AddCustomerDriverCommand parse(String args) throws ParseException {
+    public AddDriverCommand parse(String args) throws ParseException {
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_CUSTOMER, PREFIX_DRIVER,
-                        PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
         //check all prefix are present
         if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
-                || !argMultimap.getPreamble().isEmpty()
-                || getNoOfPrefixesPresent(argMultimap, PREFIX_CUSTOMER, PREFIX_DRIVER) != 1) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddCustomerDriverCommand.MESSAGE_USAGE));
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddDriverCommand.MESSAGE_USAGE));
         }
 
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
@@ -55,16 +48,9 @@ public class AddCustomerDriverParser implements Parser<AddCustomerDriverCommand>
         Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-        Prefix foundPrefix = getPrefixPresent(argMultimap, PREFIX_CUSTOMER, PREFIX_DRIVER);
-        Person person;
+        Driver driver = new Driver(name, phone, email, address, tagList);
 
-        if (foundPrefix.getPrefixClass().equals(Customer.class.getSimpleName())) {
-            person = new Customer(name, phone, email, address, tagList);
-        } else {
-            person = new Driver(name, phone, email, address, tagList);
-        }
-
-        return new AddCustomerDriverCommand(person);
+        return new AddDriverCommand(driver);
     }
 
     /**
@@ -76,26 +62,18 @@ public class AddCustomerDriverParser implements Parser<AddCustomerDriverCommand>
     }
 
     /**
-     * Returns number of prefixes in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static int getNoOfPrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return (int) Stream.of(prefixes).filter(prefix -> argumentMultimap.getValue(prefix).isPresent()).count();
-    }
-
-    /**
-     * Returns the first prefixe in the given
+     * Returns the first prefix in the given
      * {@code ArgumentMultimap}.
      */
     private static Prefix getPrefixPresent(ArgumentMultimap argumentMultimap,
                                            Prefix... prefixes) throws ParseException {
         Optional<Prefix> prefixFound = Stream
-                                            .of(prefixes)
-                                            .filter(prefix -> argumentMultimap.getValue(prefix).isPresent())
-                                            .findFirst();
+                .of(prefixes)
+                .filter(prefix -> argumentMultimap.getValue(prefix).isPresent())
+                .findFirst();
         if (prefixFound.isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddCustomerDriverCommand.MESSAGE_USAGE));
+                    AddDriverCommand.MESSAGE_USAGE));
         }
 
         return prefixFound.get();
