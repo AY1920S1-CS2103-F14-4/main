a8863c03a842b6b7bc4c9b9f2c2a569cf462f90f
Tan_Jun_Bang <tan_jun_bang@hotmail.com>
2019-10-24 21:17:16 +0800

    Refractor AddCommand and EditCommand for Driver and Customer to take in unique id.

--- src/main/java/seedu/address/model/person/Customer.java
+++ src/main/java/seedu/address/model/person/Customer.java
@@ -19,17 +19,7 @@ public class Customer extends Person {
 
     /**
      * Every field must be present and not null.
-     *
-     * @param name
-     * @param phone
-     * @param email
-     * @param address
-     * @param tags
      */
-    public Customer(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
-        super(name, phone, email, address, tags);
-    }
-
     public Customer(int id, Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         super(name, phone, email, address, tags);
         this.id = id;
