2768f075b26b0a75124e7f5fc927e3fdce750ef5
Tianyi <42670338+tysng@users.noreply.github.com>
2019-11-01 00:22:53 +0800

    Fix driver card updating issue

--- src/main/java/seedu/address/logic/commands/AssignCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignCommand.java
@@ -84,6 +84,9 @@ public class AssignCommand extends Command {
         forceAssign(driver, task, eventTime);
 
         // TODO: update GUI
+        model.setTask(task, task);
+        model.setDriver(driver, driver);
+
         return new CommandResult(String.format(MESSAGE_ASSIGN_SUCCESS,
                 task.getId(), driver.getName().fullName, eventTime.toString()));
 
