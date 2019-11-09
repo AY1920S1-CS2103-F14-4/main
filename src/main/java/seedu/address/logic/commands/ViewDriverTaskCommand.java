ce98faa80f0677d36e94730e9e989dbb8d657729
ArgVampir <kris_.k@hotmail.com>
2019-11-09 14:42:41 +0800

    removed unused imports

--- src/main/java/seedu/address/logic/commands/ViewDriverTaskCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewDriverTaskCommand.java
@@ -2,12 +2,9 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.List;
-
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.person.Driver;
 
 /**
  * List completed delivery task allocated to the particular driver.
